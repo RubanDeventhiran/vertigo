5d51fd241b3deb0f5e16ece349b060148fdfaea3
Willem van Bergen <willem@railsdoctors.com>
2013-09-01 06:30:16 -0400

    go fmt

--- connection.go
+++ connection.go
@@ -1,120 +1,120 @@
 package vertigo
 
 import (
-  "net"
-  "crypto/tls"
-  "fmt"
+	"crypto/tls"
+	"fmt"
+	"io"
+	"net"
 )
 
 type ConnectionInfo struct {
-  Address  string
-  Username string
-  Password string
-  SslConfig *tls.Config
+	Address   string
+	Username  string
+	Password  string
+	SslConfig *tls.Config
 }
 
 type Connection struct {
-  socket net.Conn
+	socket net.Conn
 }
 
 func Connect(info *ConnectionInfo) (*Connection, error) {
-  socket, err := net.Dial("tcp", info.Address)
-  if err != nil {
-    return nil, err
-  }
+	socket, err := net.Dial("tcp", info.Address)
+	if err != nil {
+		return nil, err
+	}
 
-  var connection Connection
+	var connection Connection
 
-  if info.SslConfig != nil {
-    sslRequest := BuildMessage(0)
-    sslRequest.Write(uint32(80877103))
-    sslRequest.Send(socket)
+	if info.SslConfig != nil {
+		sslRequest := BuildMessage(0)
+		sslRequest.Write(uint32(80877103))
+		sslRequest.Send(socket)
 
-    sslResponse := make([]byte, 1)
-    socket.Read(sslResponse)
-    if sslResponse[0] == byte('S') {
-      sslSocket := tls.Client(socket, info.SslConfig)
-      if tlsError := sslSocket.Handshake(); tlsError != nil {
-        sslSocket.Close()
-        return nil, tlsError
-      }
+		sslResponse := make([]byte, 1)
+		io.ReadFull(socket, sslResponse)
+		if sslResponse[0] == byte('S') {
+			sslSocket := tls.Client(socket, info.SslConfig)
+			if tlsError := sslSocket.Handshake(); tlsError != nil {
+				sslSocket.Close()
+				return nil, tlsError
+			}
 
-      connection = Connection{sslSocket}
-    } else {
-      socket.Close()
-      return nil, fmt.Errorf("SSL not available on this server")
-    }
-  } else {
-    connection = Connection{socket}
-  }
+			connection = Connection{sslSocket}
+		} else {
+			socket.Close()
+			return nil, fmt.Errorf("SSL not available on this server")
+		}
+	} else {
+		connection = Connection{socket}
+	}
 
-  
-  if err := connection.initConnection(info); err != nil {
-    socket.Close()
-    return nil, err
-  }
+	if err := connection.initConnection(info); err != nil {
+		socket.Close()
+		return nil, err
+	}
 
-  return &connection, nil
+	return &connection, nil
 }
 
-func (c *Connection) initConnection(info *ConnectionInfo) (error) {
-  startupMessage := BuildMessage(0)
-  startupMessage.Write(ProtocolVersion)
-  startupMessage.WriteString("user")
-  startupMessage.WriteString(info.Username)
-  startupMessage.WriteNull()
-  c.sendMessage(startupMessage)
+func (c *Connection) initConnection(info *ConnectionInfo) error {
+	startupMessage := BuildMessage(0)
+	startupMessage.Write(ProtocolVersion)
+	startupMessage.WriteString("user")
+	startupMessage.WriteString(info.Username)
+	startupMessage.WriteNull()
+	c.sendMessage(startupMessage)
 
-  for {
-    nextMessage, err := c.receiveMessage()
-    if err != nil {
-      return err
-    }
+	for {
+		nextMessage, err := c.receiveMessage()
+		if err != nil {
+			return err
+		}
 
-    if nextMessage.MessageType == 'Z' {
-      return nil
-    }
-  }
+		if nextMessage.MessageType == 'Z' {
+			return nil
+		}
+	}
 }
 
-func (c *Connection) Query(sql string) (error) {
-  queryMessage := BuildMessage('Q')
-  queryMessage.WriteString(sql)
+func (c *Connection) Query(sql string) error {
+	queryMessage := BuildMessage('Q')
+	queryMessage.WriteString(sql)
 
-  if err := c.sendMessage(queryMessage); err != nil {
-    return err
-  }
+	if err := c.sendMessage(queryMessage); err != nil {
+		return err
+	}
 
-  for {
-    nextMessage, err := c.receiveMessage()
-    if err != nil {
-      return err;
-    }
+	for {
+		nextMessage, err := c.receiveMessage()
+		if err != nil {
+			return err
+		}
 
-    if nextMessage.MessageType == 'Z' {
-      return nil
-    }
-  }
+		if nextMessage.MessageType == 'Z' {
+			return nil
+		}
+	}
 }
 
-func (c *Connection) Close() (error) {
-  terminateMessage := BuildMessage('X')
-  c.sendMessage(terminateMessage)
-  c.socket.Close()
-  return nil
+func (c *Connection) Close() error {
+	terminateMessage := BuildMessage('X')
+	c.sendMessage(terminateMessage)
+	c.socket.Close()
+	return nil
 }
 
-func (c *Connection) sendMessage(m Message) (error) {
-  m.Printf()
-  return m.Send(c.socket)
+func (c *Connection) sendMessage(m Message) error {
+	m.Printf()
+	return m.Send(c.socket)
 }
 
 func (c *Connection) receiveMessage() (*Message, error) {
-  nextMessage, err := ReadMessage(c.socket)
-  if err!= nil {
-    return nil, err
-  } else {
-    nextMessage.Printf()
-    return nextMessage, nil  
-  }
+	nextMessage, err := ReadMessage(c.socket)
+	if err != nil {
+		return nil, err
+	} else {
+		nextMessage.Printf()
+		return nextMessage, nil
+	}
 }
